1ab3756b1e9001742e5493dd6d8eabc526f06fd7
NotDavvid <notdavvid@gmail.com>
2015-11-13 23:28:39 -0600

    User half done.

---
+++ Xcode file/Xcode file/CreationPage.h
@@ -0,0 +1,13 @@
+//
+//  CreationPage.h
+//  Xcode file
+//
+//  Created by Davvid Caballero on 11/13/15.
+//  Copyright © 2015 Davvid Caballero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreationPage : UIViewController
+
+@end
---
+++ Xcode file/Xcode file/User.h
@@ -0,0 +1,17 @@
+//
+//  User.h
+//  Xcode file
+//
+//  Created by Davvid Caballero on 11/13/15.
+//  Copyright © 2015 Davvid Caballero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+@property NSString *username;
+@property NSMutableArray *eventsCreated;
+@property NSMutableArray *eventsInvited;
+@property NSString *password;
+
+@end
