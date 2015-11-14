a30f6a9d1cbdfe206a0976bb452af3a6a263067f
usherman678 <usherman678@aol.com>
2015-11-13 23:31:44 -0600

    add layout connects

--- Xcode file/Xcode file/MainController.h
+++ Xcode file/Xcode file/MainController.h
@@ -1,16 +1,13 @@
 //
-//  ViewController.h
+//  MainController.h
 //  Xcode file
 //
-//  Created by Davvid Caballero on 11/13/15.
+//  Created by Alex Thompson on 11/13/15.
 //  Copyright © 2015 Davvid Caballero. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
-
+@interface MainController : UIViewController
 
 @end
-
