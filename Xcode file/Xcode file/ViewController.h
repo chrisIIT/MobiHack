e88133d3de99668568d17654eea7210eee1080b4
NotDavvid <notdavvid@gmail.com>
2015-11-13 22:39:38 -0600

    Made project

---
+++ Xcode file/Xcode file/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xcode file
+//
+//  Created by Davvid Caballero on 11/13/15.
+//  Copyright © 2015 Davvid Caballero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Xcode file/Xcode file/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Xcode file
+//
+//  Created by Davvid Caballero on 11/13/15.
+//  Copyright © 2015 Davvid Caballero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
